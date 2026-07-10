f97e57c56553b90d1134cbb0b287071b8e24e0be
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2026-07-10 07:06:59 -0700

    Fix doc tests

--- src/opcode.rs
+++ src/opcode.rs
@@ -6,7 +6,7 @@
 /// you may not use this file except in compliance with the License.
 /// You may obtain a copy of the License at
 ///
-///     http://www.apache.org/licenses/LICENSE-2.0
+/// http://www.apache.org/licenses/LICENSE-2.0
 ///
 /// ---
 /// Portions of this file are derived from https://github.com/DLR-FT/wasm-interpreter:
