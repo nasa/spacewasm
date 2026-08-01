873c33e6437f59fe881a2b4fddebd719ec69dd71
Kadir Can Yildirim <252162627+kadircanyildirm-crypto@users.noreply.github.com>
2026-08-01 20:21:40 +0300

    Delete the line instead of commenting it out

--- crates/spacewasi/src/wasi_preview1.rs
+++ crates/spacewasi/src/wasi_preview1.rs
@@ -373,8 +373,6 @@ pub fn make_wasi_preview1_module(wasi_ctx: wasi_common::WasiCtx) -> HostModule {
                         panic!("expected i64");
                     };
 
-                    // eprintln!("fd_fdstat_set_rights({a0}, {a1}, {a2})");
-
                     let code = block_on(wasi_snapshot_preview1::fd_fdstat_set_rights(
                         &mut *wasi_ctx_fd_fdstat_set_rights.borrow_mut(),
                         &mut GuestMemory::Shared(unsafe {
