7c34b89f69393c729702c7f70b433a6336e46083
Cole Wilson <Cole.B.Wilson@jpl.nasa.gov>
2026-08-21 17:13:27 -0700

    update CI script

--- crates/spacewasm_bench/scripts/run_all.py
+++ crates/spacewasm_bench/scripts/run_all.py
@@ -1,41 +1,44 @@
 import queue
 import json
+import math
 import subprocess
+import sys
+import os
+import time
 from threading import Thread
 
-triples = [
-    "riscv32i-unknown-none-elf",
-    "riscv64gc-unknown-none-elf",
-    "thumbv7m-none-eabi",
-    "thumbv7em-none-eabihf",
-]
-
-elf_sections = [
-    ".text",
-    ".rodata",
-    ".data",
-    ".bss"
-]
+qemu_commands = {
+    "riscv32i-unknown-none-elf": "qemu-system-riscv32 -icount shift=0 -bios none -cpu rv32 -machine virt -m 256M -nographic -semihosting-config enable=on,target=native -kernel",
+    "riscv64gc-unknown-none-elf": "qemu-system-riscv64 -icount shift=0 -bios none -cpu rv64 -machine virt -m 256M -nographic -semihosting-config enable=on,target=native -kernel",
+    "thumbv7m-none-eabi": "qemu-system-arm -icount shift=0 -cpu cortex-m4 -machine netduinoplus2 -nographic -semihosting-config enable=on,target=native -kernel",
+    "thumbv7em-none-eabihf": "qemu-system-arm -icount shift=0 -cpu cortex-m4 -machine netduinoplus2 -nographic -semihosting-config enable=on,target=native -kernel",
+}
+qemu_info = {
+    "riscv32i-unknown-none-elf": "`rv32` / `virt` @ 1GHz",
+    "riscv64gc-unknown-none-elf": "`rv64` / `virt` @ 1GHz",
+    "thumbv7m-none-eabi": "`cortex-m4` / `netduinoplus2` @ 1GHz",
+    "thumbv7em-none-eabihf": "`cortex-m4` / `netduinoplus2` @ 1GHz",
+}
+triples = list(qemu_commands.keys())
+elf_sections = [".text", ".rodata", ".data", ".bss"]
 
 def get_coremark(triple, q):
-    command = ["cargo", "readobj", "-q", "--release", "-p", "spacewasm_bench", "--target", triple, "--", "--sections"]
-    proc = subprocess.run(
-        command,
-        capture_output=True
-    )
+    # print("starting coremark for", f"../../target/{triple}/release/spacewasm_bench")
+    command = [*qemu_commands[triple].split(" "), f"../../target/{triple}/release/spacewasm_bench"]
 
-    output = proc.stdout.decode()
+    start_time = time.time()
+    proc = subprocess.run(command, capture_output=True)
+    full_time = time.time() - start_time
 
-    q.put((triple, output))
+    output = float(proc.stdout.decode())
+
+    q.put((triple, output, full_time))
 
 def get_sizes(triple):
     out = {}
 
     command = ["cargo", "readobj", "-q", "--release", "-p", "spacewasm_bench", "--target", triple, "--", "--sections"]
-    proc = subprocess.run(
-        command,
-        capture_output=True
-    )
+    proc = subprocess.run(command, capture_output=True)
 
     out["details"] = proc.stdout.decode()
 
@@ -53,39 +56,51 @@ def get_sizes(triple):
 
     return out
 
+def colorize(new, old, lower_better):
+    color = "normalcolor"
+    change_text = ""
+
+    if old != new:
+        if old == 0:
+            pct_change = math.inf if new > 0 else -math.inf
+        else:
+            pct_change = round(100 * (new - old) / old, 1)
+        if pct_change > 0:
+            color = "red" if lower_better else "green"
+            change_text = r"(↑" + str(pct_change) + r"% from " + str(old) + ")"
+        else:
+            color = "green" if lower_better else "red"
+            change_text = r"(↓" + str(-pct_change) + r"% from " + str(old) + ")"
+
+    return r"$${\color{" + color + r"}{" + str(new) + r"}}$$<br>"+change_text
 
 def main():
+    if len(sys.argv) != 2:
+        print(f"usage: {sys.argv[0]} <save.json>")
+        return 1
+
+    old_data = {}
+    if os.path.exists(sys.argv[1]):
+        old_data = json.load(open(sys.argv[1], "r+"))
+
     q = queue.Queue()
-    threads = [Thread(target=get_sizes, args=(i, q)) for i in triples]
-    for i in threads: i.start()
+    threads = [Thread(target=get_coremark, args=(i, q)) for i in triples]
 
     data = {}
-    old_data = {'riscv32i-unknown-none-elf': {'.text': 435, '.rodata': 5093, '.data': 0, '.bss': 130388}, 'riscv64gc-unknown-none-elf': {'.text': 9800, '.rodata': 52800, '.data': 0, '.bss': 1004224}, 'thumbv7m-none-eabi': {'.text': 2252, '.rodata': 49228, '.data': 0, '.bss': 1050404}, 'thumbv7em-none-eabihf': {'.text': 248, '.rodata': 49528, '.data': 0, '.bss': 10043304}}
+
     for triple in triples:
         data[triple] = get_sizes(triple)
 
     print("# ELF Section Sizes")
-    print("||" + "|".join(map(lambda i: f"`{i}`", triples)) + "|")
+    print("||" + "|".join(map(lambda i: f"`{i}`", elf_sections)) + "|")
     print("|--:|" + ":-:|" * len(triples))
-    for section_name in elf_sections:
-        print(f"|`{section_name}`|", end="")
-        for triple in triples:
+    for triple in triples:
+        print(f"|**`{triple}`**|", end="")
+        for section_name in elf_sections:
             size = data[triple][section_name]
-            old_size = old_data[triple][section_name]
+            old_size = old_data.get(triple, {}).get(section_name, size)
 
-            color = "normalcolor"
-            change_text = ""
-
-            if old_size != size:
-                pct_change = round(100 * (size - old_size) / old_size, 1)
-                if pct_change > 0:
-                    color = "red"
-                    change_text = r"\ (\uparrow " + str(abs(pct_change)) + r" \\%\ \text{from}\ " + str(old_size) + ")"
-                else:
-                    color = "green"
-                    change_text = r"\ (\downarrow " + str(abs(pct_change)) + r" \\%\ \text{from}\ " + str(old_size) + ")"
-
-            print(r"$${\color{" + color + r"}{" + str(size) + change_text + r"}}$$|", end="")
+            print(colorize(size, old_size, True), end="|")
         print()
     print("<details><summary><i>view detailed section sizes...</i></summary>\n")
     for triple in triples:
@@ -94,10 +109,27 @@ def main():
     print("</details>\n")
 
     print("# Coremark Scores")
-    for i in threads: i.join()
-    while q.not_empty:
-        print(q.get())
 
+    sys.stdout.flush()
+
+    for i in threads: i.start()
+    for i in threads: i.join()
+    while not q.empty():
+        triple, score, t = q.get()
+        data[triple]["coremark"] = score
+        data[triple]["coremark_time"] = t
+
+    print(f"||QEMU CPU / Board|Coremark Score|Host Time|")
+    print(f"|--:|:-:|:-:|:-:|")
+    for triple in triples:
+        score = data[triple]["coremark"]
+        t = data[triple]["coremark_time"]
+        old_score = old_data.get(triple, {}).get("coremark", score)
+        print(f"|**`{triple}`**|{qemu_info[triple]}|{colorize(score, old_score, False)}|{round(t, 2)} s|")
+    print()
+
+    with open(sys.argv[1], "w+") as f:
+        json.dump(data, f, indent=2)
 
     return 0
 
