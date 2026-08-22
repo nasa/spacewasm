d236534d3e3612bb42d4f42edd5cc16c0d11fda6
Cole Wilson <Cole.B.Wilson@jpl.nasa.gov>
2026-08-21 18:04:27 -0700

    workflow changes

--- crates/spacewasm_bench/scripts/run_all.py
+++ crates/spacewasm_bench/scripts/run_all.py
@@ -23,8 +23,7 @@ triples = list(qemu_commands.keys())
 elf_sections = [".text", ".rodata", ".data", ".bss"]
 
 def get_coremark(triple, q):
-    # print("starting coremark for", f"../../target/{triple}/release/spacewasm_bench")
-    command = [*qemu_commands[triple].split(" "), f"../../target/{triple}/release/spacewasm_bench"]
+    command = [*qemu_commands[triple].split(" "), f"target/{triple}/release/spacewasm_bench"]
 
     start_time = time.time()
     proc = subprocess.run(command, capture_output=True)
@@ -56,33 +55,7 @@ def get_sizes(triple):
 
     return out
 
-def colorize(new, old, lower_better):
-    color = "normalcolor"
-    change_text = ""
-
-    if old != new:
-        if old == 0:
-            pct_change = math.inf if new > 0 else -math.inf
-        else:
-            pct_change = round(100 * (new - old) / old, 1)
-        if pct_change > 0:
-            color = "red" if lower_better else "green"
-            change_text = r"(↑" + str(pct_change) + r"% from " + str(old) + ")"
-        else:
-            color = "green" if lower_better else "red"
-            change_text = r"(↓" + str(-pct_change) + r"% from " + str(old) + ")"
-
-    return r"$${\color{" + color + r"}{" + str(new) + r"}}$$<br>"+change_text
-
 def main():
-    if len(sys.argv) != 2:
-        print(f"usage: {sys.argv[0]} <save.json>")
-        return 1
-
-    old_data = {}
-    if os.path.exists(sys.argv[1]):
-        old_data = json.load(open(sys.argv[1], "r+"))
-
     q = queue.Queue()
     threads = [Thread(target=get_coremark, args=(i, q)) for i in triples]
 
@@ -91,46 +64,18 @@ def main():
     for triple in triples:
         data[triple] = get_sizes(triple)
 
-    print("# ELF Section Sizes")
-    print("||" + "|".join(map(lambda i: f"`{i}`", elf_sections)) + "|")
-    print("|--:|" + ":-:|" * len(triples))
-    for triple in triples:
-        print(f"|**`{triple}`**|", end="")
-        for section_name in elf_sections:
-            size = data[triple][section_name]
-            old_size = old_data.get(triple, {}).get(section_name, size)
-
-            print(colorize(size, old_size, True), end="|")
-        print()
-    print("<details><summary><i>view detailed section sizes...</i></summary>\n")
-    for triple in triples:
-        print(f"### `{triple}`")
-        print(f"```\n{data[triple]["details"]}\n```\n")
-    print("</details>\n")
-
-    print("# Coremark Scores")
-
-    sys.stdout.flush()
-
     for i in threads: i.start()
     for i in threads: i.join()
     while not q.empty():
         triple, score, t = q.get()
         data[triple]["coremark"] = score
         data[triple]["coremark_time"] = t
+        data[triple]["qemu_info"] = qemu_info[triple]
 
-    print(f"||QEMU CPU / Board|Coremark Score|Host Time|")
-    print(f"|--:|:-:|:-:|:-:|")
-    for triple in triples:
-        score = data[triple]["coremark"]
-        t = data[triple]["coremark_time"]
-        old_score = old_data.get(triple, {}).get("coremark", score)
-        print(f"|**`{triple}`**|{qemu_info[triple]}|{colorize(score, old_score, False)}|{round(t, 2)} s|")
-    print()
+    data["triples"] = triples
+    data["elf_sections"] = elf_sections
 
-    with open(sys.argv[1], "w+") as f:
-        json.dump(data, f, indent=2)
-
+    print(json.dumps(data, indent=2))
     return 0
 
 if __name__ == "__main__":
